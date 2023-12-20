43759ff25e86adc50eb51356734ff66896baebd8
jiankh <131821436+jiankh@users.noreply.github.com>
2023-12-20 17:01:57 -0500

    barebones of app finished

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+
+const messages = [
+    {
+      text: "Hi there!",
+      user: "Amando",
+      added: new Date()
+    },
+    {
+      text: "Hello World!",
+      user: "Charles",
+      added: new Date()
+    }
+];
+
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Message Board', messages:messages });
 });
+
+router.get("/new", function(req,res,next) {
+    res.render('form')
+} )
+
+router.post("/new", function(req,res,next) {
+    messages.push({
+        text: req.body.message,
+        user: req.body.author,
+        added: new Date()
+    })
+    res.redirect("/")
+})
 
 module.exports = router;
